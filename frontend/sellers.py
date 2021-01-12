4594f72af0740a1fcd72eb594a81b8c0d0b2c8c3
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-12 18:34:52 -0300

    Adaptação de recebimento de form para recebimento de objeto

--- frontend/sellers.py
+++ frontend/sellers.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, Blueprint
 from backend.controllers.seller import create_seller, list_sellers
-
+from backend.models.seller import Seller
 
 seller = Blueprint(__name__, 'seller')
 
@@ -10,14 +10,14 @@ def seller_form():
     return render_template('seller/form_seller.html', titulo='Seller')
 
 
-@seller.route('/seller', methods=['POST'])
-def seller_create():
-    seller = request.form
-    create_seller(seller)
-    return redirect('/seller')
-
-
 @seller.route('/seller', methods=['GET'])
 def seller_list_():
     sellers = list_sellers()
     return render_template('seller/list_seller.html', title='Sellers', data=sellers)
+
+
+@seller.route('/seller', methods=['POST'])
+def seller_create():
+    seller = Seller(request.form.get('nome'), request.form.get('telefone'), request.form.get('email'))
+    create_seller(seller)
+    return redirect('/seller')
