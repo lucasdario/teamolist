e9e4a4fa69a9275c54c1026d1e5b0c32e05514a8
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-13 16:24:00 -0300

    Criadas funções de edição e de exclusão na controller de categoria

---
+++ backend/controllers/controller_category.py
@@ -0,0 +1,28 @@
+from backend.dao.db.dao_category import read_categories, write_category, delete_category, update_category
+from backend.controllers.log import create_log
+from backend.models.category import Category
+from backend.models.log import Log
+
+
+def create_category(category: Category):
+    write_category(category)
+    log = Log('Created Category')
+    create_log(log)
+
+
+def list_categories() -> list:
+    categories = read_categories()
+    log = Log('Listed Categories')
+    create_log(log)
+    return categories
+
+def remove_category(id: int):
+    delete_category(id)
+    log = Log('Deleted Category')
+    create_log(log)
+
+def edit_category(id: int, name: str, description: str):
+    category = Category(name, description, id)
+    update_category(category)
+    log = Log('Updated Category')
+    create_log(log)
