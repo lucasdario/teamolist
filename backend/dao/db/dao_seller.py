c37bef896f01a2d6147d2297611e5e7795e7a156
olgaleao <olgaleao@gmail.com>
2021-01-15 09:20:22 -0400

    update and delete sellers

--- backend/dao/db/dao_seller.py
+++ backend/dao/db/dao_seller.py
@@ -3,7 +3,8 @@ from backend.models.seller import Seller
 
 
 def write_seller(seller: Seller):
-    cursor.execute(f"insert into sellers (name, email, phone) values ('{seller.name}', '{seller.email}', '{seller.phone}');")
+    cursor.execute(
+        f"insert into sellers (name, phone, email) values ('{seller.name}', '{seller.phone}', '{seller.email}');")
     conn.commit()
 
 
@@ -15,3 +16,18 @@ def read_sellers() -> list:
         seller = Seller(item[1], item[2], item[3], item[0])
         sellers.append(seller)
     return sellers
+
+
+def update_seller(seller: Seller) -> None:
+    cursor.execute(f"""
+                    UPDATE sellers SET name='{seller.name}',
+                    phone='{seller.phone}',
+                    email='{seller.email}'
+                    WHERE sellers.id={seller.id}
+                    """)
+    conn.commit()
+
+
+def delete_seller(id: int) -> None:
+    cursor.execute(f"DELETE FROM sellers WHERE sellers.id={id}")
+    conn.commit()
--- frontend/sellers.py
+++ frontend/sellers.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, Blueprint
-from backend.controllers.seller import create_seller, list_sellers
+from backend.controllers.controller_seller import create_seller, list_sellers, edit_seller, remove_seller
 from backend.models.seller import Seller
 
 seller = Blueprint(__name__, 'seller')
@@ -10,14 +10,44 @@ def seller_form():
     return render_template('seller/form_seller.html', titulo='Seller')
 
 
+@seller.route('/seller', methods=['POST'])
+def seller_create():
+    seller = Seller(request.form.get('nome'), request.form.get('phone'), request.form.get('email'))
+    create_seller(seller)
+    return redirect('/seller')
+
+
 @seller.route('/seller', methods=['GET'])
 def seller_list_():
     sellers = list_sellers()
     return render_template('seller/list_seller.html', title='Sellers', data=sellers)
 
 
-@seller.route('/seller', methods=['POST'])
-def seller_create():
-    seller = Seller(request.form.get('nome'), request.form.get('telefone'), request.form.get('email'))
-    create_seller(seller)
+@seller.route('/seller/update')
+def seller_update():
+    id = request.args.get('id')
+    name = request.args.get('name')
+    phone = request.args.get('phone')
+    email = request.args.get('email')
+
+    return render_template('seller/form_seller.html', titulo='Edit Seller',
+                           update=True, id=id, name=name, phone=phone, email=email)
+
+
+@seller.route('/seller/update', methods=['POST'])
+def seller_update_save():
+    id = request.form.get('id')
+    name = request.form.get('name')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+    edit_seller(id, name, phone, email)
+
+    return redirect('/seller')
+
+
+@seller.route('/seller/delete')
+def seller_delete():
+    id = request.args.get('id')
+    remove_seller(id)
+
     return redirect('/seller')
