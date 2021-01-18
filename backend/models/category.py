020339171c546b6ea798a6c221469a4fbfffeca5
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-18 11:54:50 -0300

    Adicionados property e setters para log, product e category

--- backend/models/category.py
+++ backend/models/category.py
@@ -2,6 +2,32 @@ class Category():
 
     def __init__(self, name: str, description: str, id: int = None) -> None:
   
-        self.name = name
-        self.description = description
-        self.id = id
+        self.__name = name
+        self.__description = description
+        self.__id = id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def description(self):
+        return self.__description
+
+    @property
+    def id(self):
+        return self.__id
+
+    @name.setter
+    def name(self):
+        self.__name = name
+
+    @description.setter
+    def description(self):
+        self.__description = description
+
+    @id.setter
+    def id(self):
+        self.__id = id
+
+    
--- backend/models/log.py
+++ backend/models/log.py
@@ -2,6 +2,21 @@ class Log():
 
     def __init__(self, data: str, id: int = None) -> None:
 
-        self.data = data
-        self.id = id
+        self.__data = data
+        self.__id = id
+
+    @property
+    def data(self):
+        return self.__data
         
+    @data.setter
+    def data(self):
+        self.__data = data
+    
+    @property
+    def id(self):
+        return self.__id
+        
+    @id.setter
+    def id(self):
+        self.__id = id
