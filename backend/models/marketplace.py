f25d4f3ccd919dcd85161ec93ab379204825c3b3
olgaleao <olgaleao@gmail.com>
2021-01-18 11:06:19 -0400

    add property marketplace and seller

--- backend/models/marketplace.py
+++ backend/models/marketplace.py
@@ -1,7 +1,30 @@
 class Marketplace:
 
     def __init__(self, name: str, description: str, id: int = None) -> None:
-        
-        self.name = name
-        self.description = description
-        self.id = id
+        self.__name = name
+        self.__description = description
+        self.__id = id
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def description(self) -> str:
+        return self.__description
+
+    @property
+    def id(self) -> int:
+        return self.__id
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self.__name = name
+
+    @description.setter
+    def description(self, description: str) -> None:
+        self.__description = description
+
+    @id.setter
+    def id(self, id: int) -> None:
+        self.__id = id
