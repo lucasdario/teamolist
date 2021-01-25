409080d1f6f77c36f0f44250e6b133bfbc1579de
Quesia <quesiasts@gmail.com>
2021-01-25 16:38:37 -0300

    products dao tests

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ from tests.models.tests_category import run_test_model_categories
 from tests.models.tests_product import run_test_model_product
 from tests.models.tests_seller import run_test_model_seller
 from tests.models.tests_log import run_test_model_log
+from tests.dao.test_product import run_test_dao_products
 
 
 run_test_model_marketplace()
@@ -10,4 +11,5 @@ run_test_model_product()
 run_test_model_categories()
 run_test_model_seller()
 run_test_model_log()
+run_test_dao_products()
 
