f2fb5b99e31b487ad82d1637a6db81b80365ade1
josue-avila <josue.avila@olist.com>
2021-01-25 16:06:07 -0300

    adding tests do dao log

--- run_tests.py
+++ run_tests.py
@@ -3,11 +3,15 @@ from tests.models.tests_category import run_test_model_categories
 from tests.models.tests_product import run_test_model_product
 from tests.models.tests_seller import run_test_model_seller
 from tests.models.tests_log import run_test_model_log
+from tests.dao.tests_log import run_test_dao_log
+from dotenv import load_dotenv
 
+load_dotenv()
 
 run_test_model_marketplace()
 run_test_model_product()
 run_test_model_categories()
 run_test_model_seller()
 run_test_model_log()
+run_test_dao_log()
 
