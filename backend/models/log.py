0ea8550da3513ebcdcbcffc72b8e36abbee68c4b
Matheus Abreu <matheus.abreu@olist.com>
2021-01-26 15:07:04 -0300

    Added Log model validation

--- backend/models/log.py
+++ backend/models/log.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from sqlalchemy import Column, String
+from sqlalchemy.orm import validates
+
 from backend.models.base_model import BaseModel
 
 
@@ -8,3 +12,11 @@ class Log(BaseModel):
 
     def __init__(self, data: str) -> None:
         self.data = data
+
+    @validates('data')
+    def validate_data(self, _, data):
+        if not data:
+            raise ValueError('You cannot create an empty log.')
+        date = datetime.now()
+        data = date.strftime(f"%d/%m/%Y às %H:%M:%S => {data}")
+        return data
