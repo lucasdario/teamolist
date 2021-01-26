6782ccd798f73293b5e420fd0f67c30a337394c6
Matheus Abreu <matheus.abreu@olist.com>
2021-01-26 15:08:24 -0300

    Added new log dao test cases

--- tests/dao/tests_log.py
+++ tests/dao/tests_log.py
@@ -1,11 +1,12 @@
-from backend.dao.db.dao_base import BaseDao
-from backend.dao.db.dao_log import LogDao
-from backend.models.log import Log
-from backend.dao.db.session import Session
 from sqlalchemy.orm.session import Session as AlchemySession
 
+from backend.dao.db.dao_base import BaseDao
+from backend.dao.db.dao_log import LogDao
+from backend.dao.db.session import Session
+from backend.models.log import Log
 
-def test_log_instance():
+
+def test_log_dao_instance():
     dao = LogDao()
     assert isinstance(dao, LogDao)
     assert isinstance(dao, BaseDao)
@@ -20,7 +21,7 @@ def test_read_all():
 
 def test_save():
     dao = LogDao()
-    data = '25/01/2021 às 13:37:20 => Listed Tests'
+    data = 'Listed Tests'
     log = Log(data)
     returned_id = dao.save(log)
     saved_log = dao.read_by_id(returned_id)
@@ -28,6 +29,38 @@ def test_save():
     assert isinstance(log, Log)
     assert isinstance(saved_log, Log)
     assert log.data == saved_log.data
+
+
+def test_save_existing_log_should_raise_exception():
+    dao = LogDao()
+    data = 'Listed Tests'
+    log = Log(data)
+    returned_id = dao.save(log)
+    saved_log = dao.read_by_id(returned_id)
+
+    saved_log.data = 'Test'
+
+    try:
+        dao.save(saved_log)
+        raise AssertionError('PermissionError exception not raised!')
+    except PermissionError as e:
+        assert isinstance(e, PermissionError)
+        assert e.args == ('You cannot modify a Log.',)
+
+
+def test_delete_log_should_raise_exception():
+    dao = LogDao()
+    data = 'Listed Tests'
+    log = Log(data)
+    returned_id = dao.save(log)
+    saved_log = dao.read_by_id(returned_id)
+
+    try:
+        dao.delete(saved_log)
+        raise AssertionError('PermissionError exception not raised!')
+    except PermissionError as e:
+        assert isinstance(e, PermissionError)
+        assert e.args == ('You cannot delete a Log.',)
 
 
 def test_session():
@@ -39,8 +72,10 @@ def test_session():
 
 def run_test_dao_log():
     try:
-        test_log_instance()
+        test_log_dao_instance()
         test_save()
+        test_save_existing_log_should_raise_exception()
+        test_delete_log_should_raise_exception()
         test_read_all()
         test_session()
 
