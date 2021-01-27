0eab4da99e88d8811331f494abc27d977ef117a4
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-27 09:22:28 -0300

    Added category model validation and category model validation tests

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, String
+from sqlalchemy.orm import validates
 from backend.models.base_model import BaseModel
+import re
 
 class Category(BaseModel):
     __tablename__ = 'categories'
@@ -9,3 +11,18 @@ class Category(BaseModel):
     def __init__(self, name: str, description: str) -> None:
         self.name = name
         self.description = description
+
+    @validates('name')
+    def validate_name(self, key, name):
+        assert name != '', 'Name cannot be empty'
+        if len(name) > 200:
+            raise ValueError('Name must be less than 200 characters!')
+        if not re.match(r'^[a-zà-úA-ZÀ-Ú0-9 ]+$', name):
+            raise ValueError('Invalid characteres in name!')
+        return name
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if len(description) > 400:
+            raise ValueError('Name must be less than 200 characters!')
+        return description
--- tests/models/tests_category.py
+++ tests/models/tests_category.py
@@ -1,3 +1,4 @@
+import re
 from backend.models.base_model import BaseModel
 from backend.models.category import Category
 
@@ -10,6 +11,7 @@ def run_test_model_categories():
         obj_instace()
         obj_types()
         obj_values()
+        obj_exceptions()
         print('\033[42;1;30m'+'all model.category tests PASSED'+'\033[0;0m')
     except AssertionError as asserterror:
         print('\033[41;1;37m' +
@@ -38,3 +40,49 @@ def obj_values():
     obj = obj_instace()
     assert obj.name == name_, '[in model test] A saída de "name" não é válida ou igual a entrada'
     assert obj.description == description_, '[in model test] A saída de "description" não é válida ou igual a entrada'
+    assert obj.name != '', '[in model test] A saída de "name" está vazia'
+    assert len(obj.name) <= 200, '[in model test] A saída de "name" contém mais de 200 caracteres'
+    assert re.match(r'^[a-zà-úA-ZÀ-Ú0-9 ]+$', obj.name), '[in model test] A saída de "name" contém caracteres especiais'
+    assert len(obj.description) <= 400, '[in model test] A saída de "description" contém mais de 400 caracteres'
+
+def obj_exceptions():
+    obj_empty_name_exception()
+    obj_special_character_in_name_exception()
+    obj_name_lenght_exception()
+    obj_description_lenght_exception()
+
+
+def obj_empty_name_exception():
+    try:
+        obj = Category('', description_)
+        raise NotImplementedError('Exception not raised!')
+    except Exception as e:
+        assert isinstance(e, AssertionError), '[in model test] A exceção da validação do nome nao é do tipo esperado'
+        assert e.args == ('Name cannot be empty',), '[in model test] A saída de "name" não é válida ou igual a entrada'
+
+
+def obj_special_character_in_name_exception():
+    try:
+        obj = Category('@', description_)
+        raise NotImplementedError('Exception not raised!')
+    except ValueError as e:
+        assert isinstance(e, ValueError), '[in model test] A exceção da validação do nome não é do tipo esperado'
+        assert e.args == ('Invalid characteres in name!',), '[in model test] A saida da exceção da validação do nome não é a esperada'
+
+
+def obj_name_lenght_exception():
+    try:
+        obj = Category('n' * 201, description_)
+        raise NotImplementedError('Exception not raised!')
+    except ValueError as e:
+        assert isinstance(e, ValueError), '[in model test] A exceção da validação do nome não é do tipo esperado'
+        assert e.args == ('Name must be less than 200 characters!',), '[in model test] A saida da exceção da validação do nome não é a esperada'
+
+
+def obj_description_lenght_exception():
+    try:
+        obj = Category(name_, 'n' * 401)
+        raise NotImplementedError('Exception not raised!')
+    except ValueError as e:
+        assert isinstance(e, ValueError), '[in model test] A exceção da validação da descrição não é do tipo esperado'
+        assert e.args == ('Name must be less than 200 characters!',), '[in model test] A saida da exceção da validação da descrição não é a esperada'
