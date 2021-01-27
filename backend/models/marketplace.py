c9c2e961b1b945b374cfd5a0173e3b37efae5287
Ralph <ralph.souza@olist.com>
2021-01-27 10:10:41 +0000

    Criação de teste com validates na models de mktplaces.

--- backend/models/marketplace.py
+++ backend/models/marketplace.py
@@ -1,12 +1,30 @@
+import re
+from sqlalchemy.orm import validates
 from sqlalchemy import Column, String
 from backend.models.base_model import BaseModel
 
 
 class Marketplace(BaseModel):
     __tablename__ = 'marketplaces'
-    name = Column(String(length=200), nullable = False)
-    description = Column(String(length=600))
+    name = Column( String(length=200), nullable = False)
+    description = Column( String(length=600))
 
     def __init__(self, name: str, description: str) -> None:
         self.name = name
         self.description = description
+
+    @validates("name")
+    def validate_name(self, key, name):
+        if name == "":
+            raise ValueError("Empty name aren't valid")
+        if len(name) > 200:
+            raise ValueError("Description higher than 200 characters")
+        if not re.search(r"^[\W]+$", name):
+            raise ValueError("Name should have only letters (aA).")
+        return name
+
+    @validates("description")
+    def validate_description(self, key, description):
+        if len(description) > 600:
+            raise ValueError("Description higher than 600 characters")
+        return description
