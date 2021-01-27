ac6445faf9c76c0b7cb7f008e43470a8b2dfb0cb
Ralph <ralph.souza@olist.com>
2021-01-27 11:51:48 +0000

    Criação de teste de tipos e validação de valores.

--- tests/models/tests_marketplace.py
+++ tests/models/tests_marketplace.py
@@ -1,3 +1,4 @@
+import re
 from sqlalchemy.sql.type_api import STRINGTYPE
 from backend.models.base_model import BaseModel
 from backend.models.marketplace import Marketplace
@@ -13,7 +14,16 @@ def test_model_marketplace_value():
     mkt2 = Marketplace('Americanas', 'Departamentos')
     assert mkt2.name == 'Americanas'
     assert mkt2.description == 'Departamentos'
-    assert type(mkt2) is STRINGTYPE, "Name is not a string: %r" % mkt2
+    assert type(mkt2) is STRINGTYPE, "Name and Description aren't strings: %r" % mkt2
+    if not mkt2.name:
+        raise ValueError("Empty name aren't valid")
+    if len(mkt2.name) > 200:
+        raise ValueError("Description higher than 200 characters")
+    if len(mkt2.description) > 600:
+        raise ValueError("Description higher than 600 characters")
+    if re.search(r"^[a-zA-Z0-9]+$", mkt2.name):
+        raise ValueError("No special characters alowed in name.")
+    return mkt2.name, mkt2.description
 
 
 def run_test_model_marketplace():
